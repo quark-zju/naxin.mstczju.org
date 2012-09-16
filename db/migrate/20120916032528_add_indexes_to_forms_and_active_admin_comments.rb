53578f6dd5f74ba3c0a7961f0e3f064d0346f875
WU Jun <quark@lihdd.net>
2012-09-16 11:40:58 +0800

    修正索引名称超过限制的问题

--- db/migrate/20120916032528_add_indexes_to_forms_and_active_admin_comments.rb
+++ db/migrate/20120916032528_add_indexes_to_forms_and_active_admin_comments.rb
@@ -1,6 +1,6 @@
 class AddIndexesToFormsAndActiveAdminComments < ActiveRecord::Migration
   def change
-    add_index :forms, [:spam, :id]
-    add_index :active_admin_comments, [:resource_type, :resource_id, :namespace]
+    add_index :forms, [:spam, :id], order: { spam: :asc, id: :desc }
+    add_index :active_admin_comments, [:resource_type, :resource_id, :namespace], name: 'index_res_and_ns'
   end
 end
