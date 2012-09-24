811414d207a10a1396db44a6c2ae126cb73ee757
WU Jun <quark@lihdd.net>
2012-09-24 17:08:19 +0800

    清理无用的方法

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,5 @@
-# coding: utf-8
-
 class PagesController < ApplicationController
   def about
 
   end
-
-  def unstaff
-    session[:staff] = nil
-    redirect_to forms_url, notice: '已取消 staff 身份'
-  end
-
 end
